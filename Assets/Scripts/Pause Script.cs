87006526edcc90c7450294281aebe7401986fc2c
HubGoal <solis_angelzr1@outlook.com>
2023-11-07 20:00:57 -0600

    Added Pause menu and other studd

---
+++ Assets/Scripts/Pause Script.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseScript : MonoBehaviour
+{
+    public GameObject pauseMenu;
+    private bool isPaused = false;
+
+    void Start()
+    {
+        pauseMenu.SetActive(false);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            isPaused = !isPaused;
+
+            if (isPaused)
+            {
+                Time.timeScale = 0f;
+                pauseMenu.SetActive(true);
+            }
+            else
+            {
+                Time.timeScale = 1f;
+                pauseMenu.SetActive(false);
+            }
+        }
+    }
+}
