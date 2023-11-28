de0a478b81ee9603c2bce9dbd2fc774b22fb6d06
HubGoal <solis_angelzr1@outlook.com>
2023-11-28 01:06:22 -0600

    Major changes to the player movement
    Changes to the player movement animations and code, also improved colliders and hitboxes.

--- Assets/Scripts/Player Movement V2.cs
+++ Assets/Scripts/Player Movement V2.cs
@@ -4,9 +4,11 @@ using UnityEngine;
 
 public class PlayerMovementV2 : MonoBehaviour
 {
-    public float speed;
-    public float JumpForce;
+    public float Speed = 10f;
+    public float JumpForce = 20f;
 
+
+    private Animator anim;
     private Rigidbody2D RGBD2D;
     private bool Orientation = true;
     private bool Jump;
@@ -15,64 +17,79 @@ public class PlayerMovementV2 : MonoBehaviour
     private float hForce = 0;
 
     private bool isDead = false;
-
     void Start()
     {
         RGBD2D = GetComponent<Rigidbody2D>();
-        groundCheck = GameObject.Find("GroundCheck").transform;
+        groundCheck = transform.Find("GroundCheck");
+        anim = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
     void Update()
     {
         if (!isDead)
         {
-            // Usar Raycast para detectar el suelo con el tag "Ground"
-            RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, 0.1f, LayerMask.GetMask("Ground"));
+            Grounded = Physics2D.Linecast(transform.position, transform.position + groundCheck.localPosition, 1 << LayerMask.NameToLayer("Ground"));
+            
+            if (Grounded )
+            {
+                anim.SetBool("Jump", false);
+            }
 
-            // Verificar si el rayo golpea algo con el tag "Ground"
-            Grounded = hit.collider != null;
 
-            if (Input.GetButtonDown("Jump") && Grounded)
+            if (Input.GetKeyDown(KeyCode.W) && Grounded)
             {
                 Jump = true;
             }
-            else if (Input.GetButtonUp("Jump"))
+            else if (Input.GetKeyUp(KeyCode.W))
             {
                 if (RGBD2D.velocity.y > 0)
                 {
                     RGBD2D.velocity = new Vector2(RGBD2D.velocity.x, RGBD2D.velocity.y * 0.5f);
                 }
             }
+            if (Input.GetButtonDown("Fire1"))
+            {
+                anim.SetTrigger("Shoot");
+            }
+
+            
         }
     }
+
+
 
     private void FixedUpdate()
     {
         if (!isDead)
         {
-            hForce = Input.GetAxisRaw("Horizontal");
-            RGBD2D.velocity = new Vector2(hForce * speed, RGBD2D.velocity.y);
-        }
-        else if (hForce > 0 && !Orientation)
-        {
-            Flip();
-        }
-        else if (hForce < 0 && Orientation)
-        {
-            Flip();
-        }
 
-        if (Jump)
-        {
-            Jump = false;
-            RGBD2D.AddForce(Vector2.up * JumpForce);
+            hForce = Input.GetAxisRaw("Horizontal");
+            anim.SetFloat("Speed", Mathf.Abs(hForce));
+
+            RGBD2D.velocity = new Vector2(hForce * Speed, RGBD2D.velocity.y);
+
+            if(hForce > 0 && !Orientation)
+            {
+                Flip();
+            }
+            else if(hForce < 0 && !Orientation) {
+                Flip();
+            }
+
+            if(Jump)
+            {
+                anim.SetBool("Jump", true);
+                Jump = false;
+                RGBD2D.AddForce(Vector2.up * JumpForce);
+
+            }
         }
     }
 
     void Flip()
     {
         Orientation = !Orientation;
+
         Vector3 scale = transform.localScale;
         scale.x *= -1;
         transform.localScale = scale;
